eafdeed2988bfa3cf6d602ee58bd1d08ceb36808
desrariyandi <desrarizki@gmail.com>
2024-12-06 10:48:46 +0700

    add: pindah desa form

--- app/Filament/Pages/Cetak.php
+++ app/Filament/Pages/Cetak.php
@@ -7,6 +7,6 @@ use Filament\Pages\Page;
 class Cetak extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
-
+    protected static ?int $navigationSort = 2;
     protected static string $view = 'filament.pages.cetak';
 }
--- app/Filament/Resources/DataResource.php
+++ app/Filament/Resources/DataResource.php
@@ -2,32 +2,41 @@
 
 namespace App\Filament\Resources;
 
+use stdClass;
 use Filament\Forms;
 use App\Models\Data;
+use App\Models\Desa;
 use Filament\Tables;
 use Filament\Forms\Get;
 use Filament\Forms\Form;
+use App\Models\Kecamatan;
 use Filament\Tables\Table;
 use Filament\Resources\Resource;
 use Illuminate\Support\Collection;
+use function Laravel\Prompts\select;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Section;
+use function Laravel\Prompts\textarea;
+use Filament\Forms\Components\Textarea;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Contracts\HasTable;
 use Filament\Forms\Components\TextInput;
+
 use Filament\Forms\Components\DatePicker;
 use Illuminate\Database\Eloquent\Builder;
 use App\Filament\Resources\DataResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use App\Filament\Resources\DataResource\RelationManagers;
-use App\Models\Desa;
-use App\Models\Kecamatan;
-
-use function Laravel\Prompts\select;
 
 class DataResource extends Resource
 {
     protected static ?string $model = Data::class;
-
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $modelLabel = 'Pindah Alamat';
+    protected static ?string $navigationLabel = 'Pindah Alamat';
+    protected static ?string $slug = 'pindah-alamat';
+    protected static ?string $recordTitleAttribute = 'nama';
+    protected static ?string $navigationIcon = 'heroicon-o-arrows-right-left';
+    protected static ?int $navigationSort = 1;
 
     public static function form(Form $form): Form
     {
@@ -44,6 +53,9 @@ class DataResource extends Resource
                         TextInput::make('nik')
                             ->label('Nomor Induk Kependudukan')
                             ->numeric()
+                            ->required(),
+                        TextInput::make('nama')
+                            ->label('Nama')
                             ->required(),
                         TextInput::make('tempat_lahir')
                             ->label('Tempat Lahir')
@@ -76,12 +88,13 @@ class DataResource extends Resource
                         TextInput::make('kabupaten_asal')
                             ->label('Kabupaten Asal')
                             ->default('Kepulauan Meranti')
-                            ->disabled()
+                            ->readOnly()
                             ->required(),
                         Select::make('kecamatan_asal_id')
                             ->label('Kecamatan Asal')
                             ->dehydrated()
                             ->required()
+                            ->live()
                             ->options(fn(Get $get): Collection => Kecamatan::query()
                                 ->orderBy('name')
                                 ->pluck('name', 'id')),
@@ -93,6 +106,10 @@ class DataResource extends Resource
                                 ->where('kecamatan_id', $get('kecamatan_asal_id'))
                                 ->orderBy('name')
                                 ->pluck('name', 'id')),
+                        Textarea::make('alamat_asal')
+                            ->label('Alamat Asal')
+                            ->rows(12)
+                            ->required()
                     ]),
                 Section::make('Alamat Tujuan')
                     ->columnSpan(1)
@@ -100,8 +117,28 @@ class DataResource extends Resource
                         TextInput::make('kabupaten_tujuan')
                             ->label('Kabupaten Tujuan')
                             ->default('Kepulauan Meranti')
-                            ->disabled()
+                            ->readOnly()
+                            ->required(),
+                        Select::make('kecamatan_tujuan_id')
+                            ->label('Kecamatan Tujuan')
+                            ->dehydrated()
                             ->required()
+                            ->live()
+                            ->options(fn(Get $get): Collection => Kecamatan::query()
+                                ->orderBy('name')
+                                ->pluck('name', 'id')),
+                        Select::make('desa_tujuan_id')
+                            ->label('Desa Tujuan')
+                            ->dehydrated()
+                            ->required()
+                            ->options(fn(Get $get): Collection => Desa::query()
+                                ->where('kecamatan_id', $get('kecamatan_tujuan_id'))
+                                ->orderBy('name')
+                                ->pluck('name', 'id')),
+                        Textarea::make('alamat_tujuan')
+                            ->label('Alamat Tujuan')
+                            ->required()
+                            ->rows(12)
                     ])
             ]);
     }
@@ -110,13 +147,38 @@ class DataResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('No')->state(
+                    static function (HasTable $livewire, stdClass $rowLoop): string {
+                        return (string) ($rowLoop->iteration +
+                            ($livewire->getTableRecordsPerPage() * ($livewire->getTablePage() - 1
+                            ))
+                        );
+                    }
+                ),
+                TextColumn::make('nama')
+                    ->label('Nama')
+                    ->searchable()
+                    ->sortable()
+                    ->forceSearchCaseInsensitive(),
+                TextColumn::make('alamat_asal')
+                    ->label('Alamat Asal')
+                    ->wrap()
+                    ->formatStateUsing(function ($record) {
+                        return "Kecamatan {$record->kecamatan_asal->name}, Desa/Kelurahan {$record->desa_asal->name}, {$record->alamat_asal}";
+                    }),
+                TextColumn::make('alamat_tujuan')
+                    ->label('Alamat Tujuan')
+                    ->wrap()
+                    ->formatStateUsing(function ($record) {
+                        return "Kecamatan {$record->kecamatan_tujuan->name}, Desa/Kelurahan {$record->desa_tujuan->name}, {$record->alamat_asal}";
+                    }),
             ])
             ->filters([
                 //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
