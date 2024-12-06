98e3889697b99ccec294c74e5a54d61e1c375f8a
desrariyandi <desrarizki@gmail.com>
2024-12-06 15:35:05 +0700

    add: shield

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -50,6 +50,9 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+            ])
+            ->plugins([
+                \BezhanSalleh\FilamentShield\FilamentShieldPlugin::make()
             ]);
     }
 }
