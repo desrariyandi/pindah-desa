cdd5fbac51f218b8524c533c1cf992e4b950207a
desrariyandi <desrarizki@gmail.com>
2025-01-22 09:42:07 +0700

    fix cetak user admin

--- app/Http/Controllers/CetakController.php
+++ app/Http/Controllers/CetakController.php
@@ -12,33 +12,36 @@ class CetakController extends Controller
     public function generateReport(Request $request)
     {
         $user = Auth::user();
-        $desaId = $user->desa_id;
-        $filter = $request->input('filter', 'tiba');
-        $month = $request->input('month');
-        $year = $request->input('year');
-
+        $filter = $request->input('filter', 'all');
         $dataQuery = Data::query();
 
-        if ($month) {
-            $dataQuery->whereMonth('created_at', $month);
+
+        if ($user->hasRole('super_admin')) {
+            if ($filter === 'tiba') {
+                $dataQuery->whereNotNull('desa_tujuan_id');
+            } elseif ($filter === 'pergi') {
+                $dataQuery->whereNotNull('desa_asal_id');
+            }
+        } else {
+            if ($filter === 'tiba') {
+                $dataQuery->where('desa_tujuan_id', $user->desa_id);
+            } elseif ($filter === 'pergi') {
+                $dataQuery->where('desa_asal_id', $user->desa_id);
+            }
         }
 
-        if ($year) {
-            $dataQuery->whereYear('created_at', $year);
-        }
-
-        if ($filter === 'tiba') {
-            $dataQuery->where('desa_tujuan_id', $desaId);
-        } elseif ($filter === 'pergi') {
-            $dataQuery->where('desa_asal_id', $desaId);
-        }
-
-        $data = $dataQuery->get();
+        $data = $dataQuery->when($request->year, function ($query, $year) {
+            $query->whereYear('created_at', $year);
+        })
+            ->when($request->month, function ($query, $month) {
+                $query->whereMonth('created_at', $month);
+            })
+            ->get();
 
         $pdf = PDF::loadView('pdf.cetak', [
             'data' => $data,
-            'month' => $month,
-            'year' => $year,
+            'month' => $request->month,
+            'year' => $request->year,
             'filter' => $filter,
         ]);
 
