7e727b59b157330a7fc31f1cdaefcd77c3f261b0
desrariyandi <desrarizki@gmail.com>
2025-01-28 20:38:05 +0700

    add: custom dashboard

---
+++ resources/views/filament/pages/dashboard.blade.php
@@ -0,0 +1,5 @@
+<x-filament::page>
+    <div class="flex flex-col items-center">
+        <img src="{{ asset('images/dashboard-images.jpeg') }}" alt="Dashboard Image" class="w-48 h-auto">
+    </div>
+</x-filament::page>
